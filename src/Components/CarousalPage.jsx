362e4edb3d91eef7e11556ac39abb1de2f38b0f1
ayu-sharma <contactayusharma@gmail.com>
2024-08-04 19:26:26 +0530

    phone call started

--- src/Components/CarousalPage.jsx
+++ src/Components/CarousalPage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-function CarousalPage({images, interval = 2000, controls = false, fade = true}) {
+function CarousalPage({ images, interval = 2000, controls = false, fade = true }) {
   return (
-    <div className=''>
-      <Carousel fade={fade} controls={controls} interval={interval}>
-      {images.map((image, index) => (
-        <Carousel.Item key={index}>
-          <img src={image.src} alt={image.alt} width="100%" height="100%"/>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div>
+      <Carousel fade={fade} controls={controls} indicators={false} interval={interval}>
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img src={image.src} alt={image.alt} width="100%" height="100%" />
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
-  )
+  );
 }
 
-export default CarousalPage
+export default CarousalPage;
