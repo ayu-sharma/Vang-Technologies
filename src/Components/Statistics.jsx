fc05daec9a45d22d46ba166a7963a7ff425615e0
ayu-sharma <contactayusharma@gmail.com>
2024-08-04 21:19:37 +0530

    resolved

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -18,7 +18,7 @@ function Statistics() {
         <p class="leading-relaxed">Costumers</p>
       </div>
       <div class="p-4 sm:w-1/4 w-1/2">
-        <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">7</h2>
+        <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">4</h2>
         <p class="leading-relaxed">Products</p>
       </div>
     </div>
